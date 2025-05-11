831239b44233226319aada25b2cee84ce3933540
sangsoo <tkdtn2013@gmail.com>
2025-05-11 17:26:34 +0900

    수정

--- Formula/gogen.rb
+++ Formula/gogen.rb
@@ -2,7 +2,7 @@ class Gogen < Formula
   desc "Run restApi framework gin easily ClI gogen."
   homepage "https://github.com/tkdtn2013/gogen"
   url "https://github.com/tkdtn2013/gogen/releases/download/v1.0.0/gogen-macos-v1.0.0.tar.gz"
-  sha256 "beac0d3715432dd43de8b61b8da99828a059dd11ec8fbda5b0265f7a5126a07b"
+  sha256 "4e41b2acc972b93a6dbafe5a5d3bf8436f28cf38905710fe5e125bb801d5b077"
   license "MIT"
 
   def install
